e1f04b05b7b1b2a8cb751548990394b8d72af993
Alex I <aiparrag3@gmail.com>
2018-03-12 16:56:37 -0400

    first stage of checkCredentials

--- src/main/java/com/revature/service/Console.java
+++ src/main/java/com/revature/service/Console.java
@@ -1,11 +1,40 @@
 package com.revature.service;
 
+import java.util.Scanner;
+
+import com.revature.model.Client;
+
 public class Console implements Verification {
 
 	
 	@Override
 	public void checkCredentials() {
 		// to call the select from the repository and pass in specific values and get the table if true or return false if user is missing 
+		String username;
+		String password;
+		Client client = new Client();
+		Scanner scanner = new Scanner(System.in);
+		
+		try{
+		
+		System.out.println("Please log in.");
+		System.out.print("Username:");
+		username=scanner.nextLine();
+		System.out.println("-------------------------------------");
+		System.out.print("Password:");
+		System.out.println();
+		password = scanner.nextLine();
+		System.out.println("--------------Checking---------------");
+		
+		client.setUsername(username);
+		client.setPassword(password);
+		
+		}finally{
+			scanner.close();
+		}
+		
+		System.out.println("username: " + client.getUsername() 
+					+ " password: " + client.getPassword());
 		
 	}
 
@@ -27,4 +56,9 @@ public class Console implements Verification {
 		
 	}
 
+public static void main(String[] args) {
+	Console console = new Console();
+	console.checkCredentials();
 }
+}
+
